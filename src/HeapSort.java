7ec23503a67640f63c7f5665d7995c99ccab3607
Antonis01 <antonisanastasiou2012@gmail.com>
2022-05-16 12:59:24 +0300

    fixed heapSort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,75 +1,41 @@
 import java.net.Inet4Address;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class HeapSort {
-    public void HeapSort(ArrayList<oceanEntries> dataOcean) {
-        int Size_Array = dataOcean.size();
 
-      // Build heap (rearrange array)
-        for (int i = Size_Array / 2 - 1 ; i >= 0; i--)
-            Rearrange_Array(Main.dataOcean.size(), i);
+    static void makeHeap(ArrayList<oceanEntries> dataOcean, int k, int N){
 
+        while(2*k+1 <=N){
+            int maxson = 2*k+1;
+            if(maxson < N && dataOcean.get(maxson).getPO4uM() < dataOcean.get(maxson+1).getPO4uM())
+                maxson++;
+            if(dataOcean.get(maxson).getPO4uM() < dataOcean.get(k).getPO4uM())
+                break;
 
-        for (int i = Size_Array - 1; i > 0; i--) {
-            // Move current root to end
-
-            try {
-
-                oceanEntries temp = Main.dataOcean.get(i);
-                oceanEntries temp2 = Main.dataOcean.get(i++);
-                i--;
-                Main.dataOcean.set(i++, temp2);
-                Main.dataOcean.set(i--, temp);
-                i++;
-
-            }catch (IndexOutOfBoundsException e){
-                System.out.println(e);
-            }
-
-           // temp.equals(Main.dataOcean.get(0));
-            //Main.dataOcean.set(0, dataOcean.get(i));
-            //Main.dataOcean.get(i).equals(temp);
-
-
-            //Collections.copy(Main.dataOcean.get(0).getPO4uM() ,dataOcean.get(i).getPO4uM());
-            //Main.dataOcean.get(0).getPO4uM().equals(dataOcean.get(i).getPO4uM());
-
-
-            // call max heapify on the reduced heap
-            Rearrange_Array(i, 0);
-
+            Collections.swap(dataOcean,maxson,k);
+            k=maxson;
         }
     }
 
-    void Rearrange_Array(int Size_Array, int i) {
-        int largest = i; // Initialize largest as root
-        int leftwing = 2 * i + 1; // left = 2*i + 1
-        int rightwing = 2 * i + 2; // right = 2*i + 2
+    static void heapSort(ArrayList<oceanEntries> dataOcean, int left, int right){
+        int N= right - left, k=(N%2==0 ? N/2-1 : N/2);
+        oceanEntries temp;
 
-        // If left child is larger than root
-        if (leftwing < Size_Array && Main.dataOcean.get(leftwing).equals(Main.dataOcean.get(largest)))
-            largest = leftwing;
+        for(; k>=0; k--)
+            makeHeap(dataOcean,k,right);
 
-        // If right child is larger than largest so far
-        if (rightwing < Size_Array && Main.dataOcean.get(rightwing).equals(Main.dataOcean.get(largest)))
-            largest = rightwing;
-
-        // If largest is not root
-        if (largest != i) {
-            oceanEntries temp = Main.dataOcean.get(i);
-            Main.dataOcean.get(i).equals(Main.dataOcean.get(largest));
-            Main.dataOcean.get(largest).equals(temp);
-
-            // Recursively heapify the affected sub-tree
-            Rearrange_Array(Size_Array, largest);
+        while(right>left){
+            Collections.swap(dataOcean,left,right);
+            makeHeap(dataOcean,left, --right);
         }
     }
-
     /* A utility function to print array of size n */
     static void printHeapSort(ArrayList<oceanEntries> dO) {
         int Size_Array = dO.size();
         for (int i = 0; i < Size_Array; ++i)
-            System.out.println(dO.get(i).getPO4uM());
+            System.out.println(dO.get(i).getDate().displayDate() + " " + dO.get(i).getPO4uM());
         System.out.println();
     }
 }
