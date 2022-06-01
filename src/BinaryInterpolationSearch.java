6033fc26016d2b00f093cbdf6029e857dadeac28
Orestis6127 <orestis.zantides@gmail.com>
2022-06-01 17:35:34 +0300

    Finished Binary Interpolation Search

--- src/BinaryInterpolationSearch.java
+++ src/BinaryInterpolationSearch.java
@@ -20,57 +20,69 @@ public class BinaryInterpolationSearch {
 
         //taking the ArrayList with the data from the csv file, removing the '/' and changing the format of the date from Date to int.
         //then adding all the new dates to an int type Array
+        try {
+            int j=0;
+            for(var i: Main.dataOcean){
+                tempDateString = i.getDate().displayDate().split(splitBy);
+                oceanDates[j] = Integer.parseInt(tempDateString[0] + tempDateString[1] + tempDateString[2]);
+                j++;
+            }
 
-
-        for(int j=1; j<=1405; j++){
-            tempDateString = Main.dataOcean.get(j).getDate().displayDate().split(splitBy);
-            oceanDates[j] = Integer.parseInt(tempDateString[0] + tempDateString[1] + tempDateString[2]);
+        }catch (NumberFormatException e){
+            System.out.println(e);
         }
 
+        //Start of Binary Interpolation Search
         int i;
         int left = 0;
         int right = Main.dataOcean.size()-1;
-        double size = right - left + 1;
-        int next = (((intDate - oceanDates[left]) / (oceanDates[right]-oceanDates[left]) )*(int)size) + 1;
+        float size = right - left + 1;
+        float next = ( size * (((float)intDate-oceanDates[left])/(oceanDates[right]-oceanDates[left])) ) +1;
 
-        //Start of Binary Interpolation Search
-
-        while(intDate != oceanDates[next]){
+        while(intDate!=oceanDates[(int)next]){
             i=0;
-            size = right - left + 1;
+            size=right-left+1;
 
-            if(size <= 10){  //Apefthias Anazitisi
-                while(intDate != oceanDates[next]){
-                    if(intDate == oceanDates[next]){
-                        return "Temperature in Celsius: "+ Main.dataOcean.get(next).getT_degC() + ", Phosphate: " + Main.dataOcean.get(next).getPO4uM();
+            //APEFTHIAS--------
+            if(size<=50){
+                if(intDate >= oceanDates[(int)next]){
+                    while (true){
+                        if (intDate == oceanDates[(int) next]){
+                            return "Temperature in Celsius: " + Main.dataOcean.get((int) next).getT_degC() + " Phosphate: "+ Main.dataOcean.get((int)next).getPO4uM();
+                        }
+                            next++;
+                        }
                     }
-                    next++;
-                    if(next == 1405){
-                        return "Date Not Found";
+                else if(intDate < oceanDates[(int)next]){
+                    while (true) {
+                        if (intDate == oceanDates[(int) next]) {
+                            return "Temperature in Celsius: " + Main.dataOcean.get((int) next).getT_degC() + " Phosphate: "+ Main.dataOcean.get((int)next).getPO4uM();
+                        }
+                        next--;
                     }
+                }
+            }
+            //APEFTHIAS--------
+
+            if(intDate >= oceanDates[(int)next]){
+                while (intDate > oceanDates[(int)(next+(i*Math.sqrt((double)size)))-1]){
+                    i++;
+                }
+                right =(int)(next + i*Math.sqrt((double)size));
+                left =(int)(next + (i-1)*Math.sqrt((double)size));
+            }
+            else if(intDate < oceanDates[(int)next]){
+                while (intDate < oceanDates[(int)(next-(i*Math.sqrt((double)size)))+1]){
+                    i++;
                 }
             }
 
-            if(intDate >= oceanDates[next]){
-                while (intDate > oceanDates[(int) (next+(i*Math.sqrt(size) - 1))]){
-                    i = i + 1;
-                }
-                right =(int) (next + i*Math.sqrt(size));
-                left =(int) (next + (i-1)*Math.sqrt(size));
-            }
-            else if(intDate < oceanDates[next]){
-                while (intDate < oceanDates[(int) (next-(i*Math.sqrt(size) + 1))]){
-                    i = i + 1;
-                }
-                right =(int) (next - (i-1)*Math.sqrt(size));
-                left =(int) (next - i*Math.sqrt(size));
-            }
-            next = left + ((right - left + 1) * ((intDate-oceanDates[left])/(oceanDates[right]-oceanDates[left]))) - 1;
+            next = left + ( (float)(right-left+1)*((float)intDate-oceanDates[left])/(oceanDates[right]-oceanDates[left]) ) -1;
         }
-        if(intDate == oceanDates[next]){
-            return "Temperature in Celsius: "+ Main.dataOcean.get(next).getT_degC() + ", Phosphate: " + Main.dataOcean.get(next).getPO4uM();
+
+        if(intDate == oceanDates[(int)next]){
+            return "Temperature in Celsius: " + Main.dataOcean.get((int) next).getT_degC() + " Phosphate: "+ Main.dataOcean.get((int)next).getPO4uM();
         }
-        return  "Date Not Found";
+        else return "Not Found";
     }
-
 }
